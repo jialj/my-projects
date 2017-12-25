0189603ea7f80dffe1c831590d7b3fd4b912832c
tangwanli <771147315@qq.com>
2017-12-25 16:54:50 +0800

    update

---
+++ project/大三/try.js
@@ -0,0 +1,19 @@
+/*$(document).ready(function() {
+    $("[class*=look]").css("background","red");
+    $("[class~=look]").css("width","200px");
+    alert("hello world");
+});
+$(function() {
+    alert("hello again");
+});*/
+
+(function() {
+	var what = "dsad";
+var a = 30 + null;
+    var b = 30 + undefined;
+    if(0 == null){
+    	console.log("dsadadadsadadsad");
+    }
+    	console.log(a,b);
+
+    })();
