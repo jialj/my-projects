f6802df6f64b76a341a94ee69354188e8e6d9e5c
tangwanli <771147315@qq.com>
2018-01-15 11:27:46 +0800

    这样会发生什么

--- project/大三/try.js
+++ project/大三/try.js
@@ -7,7 +7,7 @@ $(function() {
     alert("hello again");
 });*/
 
-(function() {
+/*(function() {
 	var what = "dsad";
 var a = 30 + null;
     var b = 30 + undefined;
@@ -16,4 +16,48 @@ var a = 30 + null;
     }
     	console.log(a,b);
 
-    })();
+    })();*/
+/*    var a = "+000200px";
+    var b = 100;
+    var c = true;
+    var d = [1,2,3];
+    var e = "11px";
+    a = parseInt(a);
+    b = String(b);
+    c = c.toString();
+    d = d.toString();
+    e = e.toString();
+    console.log(a,typeof a);
+    console.log(b,typeof b);
+    console.log(c,typeof c);
+    console.log(d,typeof d);
+    console.log(e,typeof e);*/
+   
+/*   console.log(typeof foo,foo);
+
+   var foo = 0;
+   console.log(typeof foo,foo);
+
+   function foo() {console.log(1);};
+   console.log(typeof foo,foo);
+
+   var foo = 2;
+   console.log(typeof foo,foo);
+
+   function foo() {console.log(3);};
+   console.log(typeof foo,foo);*/
+   var parent = function () {
+    var len = 9;
+
+for (var i =0; i < len; i++) {
+	/*window.onload = function() {
+		console.log(i);  // 全都为9
+}*/
+     var x = function(){
+     	console.log(i);
+     }();
+
+}
+};
+parent();
+
