018d8209b49e03d12168c1193eba5471c39ed74d
tangwanli <771147315@qq.com>
2018-01-28 15:51:54 +0800

    add js封装对象

--- project/大三/try.js
+++ project/大三/try.js
@@ -60,9 +60,30 @@ for (var i =0; i < len; i++) {
 }
 };
 parent();*/
-what();
-what();
-what();
-what();
+  (function() {
+
+/*    var ul = $("div ul");
+    var li = ul.find("li");
+    var str = "+" + "200px";
+    li.animate({"top":str},1000)
+    console.log(ul.css("top"));*/
+   /* var ul = document.getElementsByTagName("ul")[0];
+    var li = document.getElementsByTagName("li")[0];
+    console.log($("img")[0].src);*/
+  /*  var aa = {
+      a : [1,2],
+      b : function() {
+        var c = this.a[0];
+        c = 2;
+        console.log(c,this.a);
+      }
+    };
+    aa.b();*/
+    $("ul").find("span");
+    var index = 0;
+    var str = "li:eq(" + index + ")";
+    console.log($("ul").find(str)[0].innerHTML);
+    
+  })();
 
 
